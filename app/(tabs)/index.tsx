078dcaee1907221d8bc04301428cd28c54cca43b
Arya A <aryaacikgoz@outlook.com>
2023-09-12 20:25:05 +0300

    Font has changed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, Platform } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Lottie from "lottie-react-native";
 
 // TODO: Logo - As web version does not support Lottie, there should be a conditional serving (as Lottie or gif) according to the platform.
 
@@ -9,20 +10,23 @@ export default function TabOneScreen() {
   return (
     <SafeAreaView>
       <ScrollView>
-        {/* <Lottie
-          autoPlay
-          loop
-          source={require("../../assets/logo/sooon.json")}
-          style={{ width: 90, height: 90, alignSelf: "center" }}
-        /> */}
-        <Image
-          source={require("../../assets/images/sooon.gif")}
-          style={{
-            width: 90,
-            height: 90,
-            alignSelf: "center",
-          }}
-        />
+        {Platform.OS === "ios" ? (
+          <Lottie
+            autoPlay
+            loop
+            source={require("../../assets/logo/sooon.json")}
+            style={{ width: 90, height: 90, alignSelf: "center" }}
+          />
+        ) : (
+          <Image
+            source={require("../../assets/images/sooon.gif")}
+            style={{
+              width: 90,
+              height: 90,
+              alignSelf: "center",
+            }}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
